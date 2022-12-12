44a889be9fc84648b4cb467fab4695911b043491
Emil <48524904+emilt050@users.noreply.github.com>
2022-12-12 21:54:31 +0200

    filters

---
+++ src/pachet/clasefilter/emil/AspiratorFilter.java
@@ -0,0 +1,30 @@
+package pachet.clasefilter.emil;
+
+import java.util.ArrayList;
+
+import pachet.clase.emil.Aspirator;
+
+public class AspiratorFilter {
+	public static ArrayList<Aspirator> filtreazaDupaCapacitateaDeColectare(ArrayList<Aspirator> listaAspiratoare,
+			int capacitateDeColectare) {
+		ArrayList<Aspirator> lista_filtrata = new ArrayList<>();
+		for (Aspirator obj : listaAspiratoare) {
+			if (obj.getCapacitateDeColectare() <= capacitateDeColectare)
+				lista_filtrata.add(obj);
+		}
+
+		return lista_filtrata;
+	}
+
+	public static ArrayList<Aspirator> filtreazaDupaClasaEmisiilorDePraf(ArrayList<Aspirator> listaAspiratoare,
+			char clasaEmisiPraf) {
+		ArrayList<Aspirator> lista_filtrata = new ArrayList<>();
+		for (Aspirator obj : listaAspiratoare) {
+			if (obj.getClasaEmisiPraf() == clasaEmisiPraf)
+				lista_filtrata.add(obj);
+		}
+
+		return lista_filtrata;
+	}
+
+}
