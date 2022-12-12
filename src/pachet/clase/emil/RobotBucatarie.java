3f9f76d239ae675f02b0a9a23957ba0bec57c0e2
Emil <48524904+emilt050@users.noreply.github.com>
2022-12-12 18:43:02 +0200

    Am modificat clasele si am adaugat generatoare

--- src/pachet/clase/emil/RobotBucatarie.java
+++ src/pachet/clase/emil/RobotBucatarie.java
@@ -4,68 +4,99 @@ package pachet.clase.emil;
 import pachet.clasa.abstracta.AparatElectric;
 
 public class RobotBucatarie extends AparatElectric {
-	private double capacitate;
-	private double numarViteze;
-	private double numarUstensile;
+	private float capacitate;
+	private int numarViteze;
+	private int numarUstensile;
+	private int nivelZgomot;
 
 	// Constructor implicit (fara parametri):
 	public RobotBucatarie() {
 		super();
-		capacitate = 0.0;
-		numarViteze = 0.0;
-		numarUstensile = 0.0;
+		this.capacitate = 0;
+		this.numarViteze = 0;
+		this.numarUstensile = 0;
+		this.nivelZgomot = 0;
 	}
 
-	// Constructor explicit (cu parametri):
+	// construcotr cu parametrii superclasei
 	public RobotBucatarie(String numeProdus, String producator, double pret, String culoare, double putereConsumata) {
 		super(numeProdus, producator, pret, culoare, putereConsumata);
+		this.capacitate = 0;
+		this.numarViteze = 0;
+		this.numarUstensile = 0;
+		this.nivelZgomot = 0;
 	}
 
+	// Constructor cu parametri:
 	public RobotBucatarie(String numeProdus, String producator, double pret, String culoare, double putereConsumata,
-			double capacitate, double numarViteze, double numarUstensile) {
+			float capacitate, int numarViteze, int numarUstensile, int nivelZgomot) {
 		super(numeProdus, producator, pret, culoare, putereConsumata);
 		this.capacitate = capacitate;
 		this.numarViteze = numarViteze;
 		this.numarUstensile = numarUstensile;
+		this.nivelZgomot = nivelZgomot;
 	}
 
 	// Constructor de copiere:
-	public RobotBucatarie(RobotBucatarie obiect) {
-		super(obiect);
-		this.capacitate = obiect.capacitate;
-		this.numarViteze = obiect.numarViteze;
-		this.numarUstensile = obiect.numarUstensile;
+	public RobotBucatarie(RobotBucatarie robotBucatarie) {
+		super(robotBucatarie);
+		this.capacitate = robotBucatarie.capacitate;
+		this.numarViteze = robotBucatarie.numarViteze;
+		this.numarUstensile = robotBucatarie.numarUstensile;
+		this.nivelZgomot = robotBucatarie.nivelZgomot;
 	}
 
-	public double getCapacitate() {
+	public float getCapacitate() {
 		return capacitate;
 	}
 
-	public void setCapacitate(double capacitate) {
+	public void setCapacitate(float capacitate) {
 		this.capacitate = capacitate;
 	}
 
-	public double getnumarViteze() {
+	public int getNumarViteze() {
 		return numarViteze;
 	}
 
-	public void setnumarViteze(double numarViteze) {
+	public void setNumarViteze(int numarViteze) {
 		this.numarViteze = numarViteze;
 	}
 
-	public double getnumarUstensile() {
+	public int getNumarUstensile() {
 		return numarUstensile;
 	}
 
-	public void setnumarUstensile(double numarUstensile) {
+	public void setNumarUstensile(int numarUstensile) {
 		this.numarUstensile = numarUstensile;
 	}
 
-	
+	public int getNivelZgomot() {
+		return nivelZgomot;
+	}
+
+	public void setNivelZgomot(int nivelZgomot) {
+		this.nivelZgomot = nivelZgomot;
+	}
+
 	@Override
 	public String toString() {
-		return super.toString() + "\n\tCapacitate:\t\t" + capacitate + " [litri]" + "\n\tNumar Viteze:\t\t"
-				+ numarViteze + "\n\tNumar Ustensile:\t" + numarUstensile;
+		StringBuilder sir = new StringBuilder();
+
+		sir.append(super.toString());
+		sir.append("\n\tCapacitate: " + this.capacitate + " litri");
+		sir.append("\n\tNumar viteze: " + this.numarViteze);
+		sir.append("\n\tNumar ustensile: " + this.numarUstensile);
+		sir.append("\n\tNivel zgomot: " + this.nivelZgomot + " [dB]\n\n");
+
+		return sir.toString();
+	}
+
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// folosim contructorul de copiere
+		// urmat de "new" pentru a creea un nou obiect
+		RobotBucatarie clona = new RobotBucatarie(this);
+		return clona;
 	}
 
 	@Override
---
+++ src/pachet/clasegeneratoare/emil/GeneratorAspiratoare.java
@@ -0,0 +1,42 @@
+package pachet.clasegeneratoare.emil;
+
+import java.util.ArrayList;
+import java.util.Random;
+import pachet.clase.emil.*;
+
+public class GeneratorAspiratoare {
+	private static String[] claseEmisiPraf = new String[] { "A", "B", "C" };
+	private static String[] tipuri = new String[] { "vertical", "horizontal" };
+	private static String[] tipuriAspirare = new String[] { "umeda", "uscata" };
+	private static String[] tipuriCarcasa = new String[] { "plastic", "metal" };
+	private static String[] tipuriSac = new String[] { "hartie", "plastic", "textil" };
+	private static String[] tipuriFiltru = new String[] { "HEPA", "H13", "H14", "H15" };
+	private static int[] capacitateDeColectare = new int[] { 1000, 2000, 3000, 4000, 5000 };
+	private static int[] greutate = new int[] { 1, 2, 3, 4, 5 };
+	private static int[] nivelZgomot = new int[] { 50, 60, 70, 80, 90 };
+	private static int[] nrFiltre = new int[] { 1, 2, 3 };
+	private static String[] culori = new String[] { "albastru", "rosu", "galben", "verde", "mov", "portocaliu", "negru",
+			"alb" };
+
+	public static ArrayList<Aspirator> genereaza(int nrAspiratoare) {
+		ArrayList<Aspirator> listaAspiratoare = new ArrayList<Aspirator>();
+		Random r = new Random();
+		for (int i = 0; i < nrAspiratoare; i++) {
+			Aspirator a = new Aspirator("Aspirator" + (i + 1), "Producator" + (i + 1), r.nextDouble(1000),
+					culori[r.nextInt(culori.length)], r.nextDouble(1000));
+			a.setClasaEmisiPraf(claseEmisiPraf[r.nextInt(claseEmisiPraf.length)]);
+			a.setTip(tipuri[r.nextInt(tipuri.length)]);
+			a.setTipAspirare(tipuriAspirare[r.nextInt(tipuriAspirare.length)]);
+			a.setTipCarcasa(tipuriCarcasa[r.nextInt(tipuriCarcasa.length)]);
+			a.setTipSac(tipuriSac[r.nextInt(tipuriSac.length)]);
+			a.setTipFiltru(tipuriFiltru[r.nextInt(tipuriFiltru.length)]);
+			a.setCapacitateDeColectare(capacitateDeColectare[r.nextInt(capacitateDeColectare.length)]);
+			a.setGreutate(greutate[r.nextInt(greutate.length)]);
+			a.setNivelZgomot(nivelZgomot[r.nextInt(nivelZgomot.length)]);
+			a.setNrFiltre(nrFiltre[r.nextInt(nrFiltre.length)]);
+			listaAspiratoare.add(a);
+		}
+		return listaAspiratoare;
+	}
+
+}
