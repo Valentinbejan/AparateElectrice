29e7bf34e1b579005f575d8e1042714b45ec2d3e
Emil <48524904+emilt050@users.noreply.github.com>
2022-12-18 20:01:37 +0200

    clase de test

--- src/pachet/teste/emil/TestAparate.java
+++ src/pachet/teste/emil/TestAparate.java
@@ -1,46 +1,19 @@
 package pachet.teste.emil;
 
-import java.util.ArrayList;
-
 import pachet.clase.emil.*;
-import pachet.clasefilter.emil.*;
-import pachet.clasegeneratoare.emil.*;
 
 public class TestAparate {
 
 	public static void main(String[] args) {
+		Aspirator aspirator = new Aspirator();
+		RobotBucatarie robotBucatarie = new RobotBucatarie();
+		Televizor televizor = new Televizor();
+		StatieCalcat statieCalcat = new StatieCalcat();
 
-		ArrayList<Aspirator> listaAspiratoare = GeneratorAspiratoare.genereaza(5);
-		for (Aspirator aspirator : listaAspiratoare) {
-			System.out.println(aspirator);
-		}
+		System.out.println("Aspirator: " + aspirator);
+		System.out.println("Robot de bucatarie: " + robotBucatarie);
+		System.out.println("Televizor: " + televizor);
+		System.out.println("Statie de calcat: " + statieCalcat);
 
-		ArrayList<Televizor> listaTelevizoare = GeneratorTelevizore.genereaza(5);
-		for (Televizor televizor : listaTelevizoare) {
-			System.out.println(televizor);
-		}
-
-		ArrayList<StatieCalcat> listaStatiiDeCalcat = GeneratorStatiiCalcat.genereaza(5);
-		for (StatieCalcat statieDeCalcat : listaStatiiDeCalcat) {
-			System.out.println(statieDeCalcat);
-		}
-
-		ArrayList<RobotBucatarie> listaRobotiDeBucatarie = GeneratorRobotiBucatarie.genereaza(5);
-		for (RobotBucatarie robotBucatarie : listaRobotiDeBucatarie) {
-			System.out.println(robotBucatarie);
-		}
-
-		// Iteram folosind programarea concurenta:
-
-//		AspiratorFilter.filtreazaDupaCapacitateaDeColectare(listaAspiratoare, 3000).forEach(System.out::println);
-//		AspiratorFilter.filtreazaDupaClasaEmisiilorDePraf(listaAspiratoare, 'A').forEach(System.out::println);
-//		RobotBucatarieFilter.filtreazaDupaNumarViteze(listaRobotiDeBucatarie, 3).forEach(System.out::println);
-//		RobotBucatarieFilter.filtreazaDupaNumarUstensile(listaRobotiDeBucatarie, 5).forEach(System.out::println);
-//		StatieCalcatFilter.filtreazaDupaMaterialTalpa(listaStatiiDeCalcat, "teflon").forEach(System.out::println);
-//		StatieCalcatFilter.filtreazaDupaFunctie(listaStatiiDeCalcat, "anticalcar").forEach(System.out::println);
-//		TelevizorFilter.filtreazaDupaTehnologieAudio(listaTelevizoare, "Dolby Atmos").forEach(System.out::println);
-//		TelevizorFilter.filtreazaDupaTipTV(listaTelevizoare, "SmartTV").forEach(System.out::println);
-		
-		
 	}
 }
